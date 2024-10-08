c8c7f6b1bf1e382fef073a2c979e8ce8e8a94750
SSaifALiZ <60507013+SSaifALiZ@users.noreply.github.com>
2024-10-08 20:35:04 +0500

    wip

--- src/app/(authorized)/dashboard/page.tsx
+++ src/app/(authorized)/dashboard/page.tsx
@@ -1,15 +1,17 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import Requests from "@/components/requests";
 import ScheduleTable from "@/components/scheduleTable";
 
 const Page = () => (
-  <div className="w-full flex flex-1 h-[90%] overflow-hidden">
-    <div className="flex flex-col min-w-[50%] max-w-full w-full">
-      <ScheduleTable />
+  <Suspense fallback={<div>Loading...</div>}>
+    <div className="w-full flex flex-1 h-[90%] overflow-hidden">
+      <div className="flex flex-col min-w-[50%] max-w-full w-full">
+        <ScheduleTable />
+      </div>
+      <Requests />
     </div>
-    <Requests />
-  </div>
+  </Suspense>
 );
 
 export default Page;
--- src/app/(authorized)/schedule/page.tsx
+++ src/app/(authorized)/schedule/page.tsx
@@ -1,14 +1,15 @@
 "use client";
-import React from "react";
-import Requests from "@/components/requests";
+import React, { Suspense } from "react";
 import ScheduleTable from "@/components/scheduleTable";
 
 const Page = () => (
-  <div className="w-full flex flex-1 h-[90%] overflow-hidden">
-    <div className="flex flex-col min-w-[50%] max-w-full w-full">
-      <ScheduleTable />
+  <Suspense fallback={<div>Loading...</div>}>
+    <div className="w-full flex flex-1 h-[90%] overflow-hidden">
+      <div className="flex flex-col min-w-[50%] max-w-full w-full">
+        <ScheduleTable />
+      </div>
     </div>
-  </div>
+  </Suspense>
 );
 
 export default Page;
